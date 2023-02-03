cb4beed407f1374eb8c15561c1598488d793ed89
DyRose <64310374+DyroS3@users.noreply.github.com>
2023-02-03 14:31:37 +0800

    Update dy_prospecting.sql

--- dy_prospecting/dy_prospecting.sql
+++ dy_prospecting/dy_prospecting.sql
@@ -1,7 +1,7 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 	('bones', '小石子', 100, 0, 1),
 	('detector', '探测仪器', 2000, 0, 1),
-	('dragon_scales', 'Dragon Scales', 1000, 0, 1),
+	('dragon_scales', '龙鳞', 1000, 0, 1),
 	('nuts', '螺母', 100, 0, 1),
 	('metalscrap', '废金属', 100, 0, 1),
 	('bolts', '螺栓', 100, 0, 1);
